acf76e7a91f522dc03e53f011f86dddd51a9af7f
Michael James <micrjames921@gmail.com>
2022-12-10 21:20:24 -0700

    updated function to use Random class methods

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const { shuffle } = require("./shuffle");
 
 const array = [...range(81)];
 console.log(array);
-const shuffledArray = shuffle(array);
-console.log(shuffledArray);
+try {
+   const shuffledArray = shuffle(array);
+   console.log(shuffledArray);
 
-const slicedAndShuffledArray = shuffledArray.slice(0, 10);
-console.log(slicedAndShuffledArray);
+   const slicedAndShuffledArray = shuffledArray.slice(0, 10);
+   console.log(slicedAndShuffledArray);
+} catch(e) {
+   console.error(e);
+}
+
--- shuffle.js
+++ shuffle.js
@@ -1,6 +1,11 @@
+const { Random } = require("../Random/Random.js");
+
 const shuffle = function(array) {
+    const randNum = new Random(0);
+    let j;
     for(let i = array.length-1; i > 0; i--) {
-	    let j = Math.floor(Math.random() * (i+1));
+		randNum.maximum = i;
+		j = randNum.integer;
 	    
 	    [array[i], array[j]] = [array[j], array[i]];
 	}
